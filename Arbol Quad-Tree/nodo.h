c4cb1b0c0dfba03ec008465f0f42d7455b1ee450
Khamarhack <143459006+Khamarhack@users.noreply.github.com>
2024-09-25 19:56:18 -0500

    Update nodo.h

--- Arbol Quad-Tree/nodo.h
+++ Arbol Quad-Tree/nodo.h
@@ -1,30 +1,34 @@
 #ifndef __NODO__H__
 #define __NODO__H__
 
+#include <utility>  // Para std::pair
+
 using namespace std;
 
-template< class T >
-class Nodo
-{
-  protected:
-    pair<T,T> dato;
+template<class T>
+class Nodo {
+protected:
+    pair<T, T> dato;
     Nodo<T>* NW;
     Nodo<T>* NE;
     Nodo<T>* SW;
     Nodo<T>* SE;
 
+public:
+    Nodo();
+    Nodo(pair<T, T> val);
 
-  public:
-  	Nodo();
-  	Nodo(pair<T,T> val);
-  	int altura();
-  	int tamano();
-    pair<T,T> obtenerDato();
-    void insertar(pair<T,T>  val);
-    void fijarDato(pair<T,T> val);
+    int altura();
+    int tamano();
+
+    pair<T, T> obtenerDato();
+    void fijarDato(pair<T, T> val);
+    
+    void insertar(pair<T, T> val);
+    Nodo* buscar(pair<T, T> val);
+
     void preOrden();
     void posOrden();
-    Nodo* buscar(pair<T,T> val);
 };
 
 #include "Nodo.hxx"
