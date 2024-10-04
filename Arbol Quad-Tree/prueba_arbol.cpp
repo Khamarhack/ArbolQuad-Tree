dc101b5bff6cf2905e39573dd7dfd139badb6c94
Khamarhack <143459006+Khamarhack@users.noreply.github.com>
2024-10-04 15:39:57 -0500

    Update prueba_arbol.cpp

--- Arbol Quad-Tree/prueba_arbol.cpp
+++ Arbol Quad-Tree/prueba_arbol.cpp
@@ -9,7 +9,38 @@ Estructuras de datos
 #include "quadtree.h"
 
 using namespace std;
+struct Point {
+    int x, y;
+    Point(int _x, int _y) : x(_x), y(_y) {}
+};
 
-int main(){
+// Estructura para representar un límite rectangular
+struct Boundary {
+    int x_min, x_max, y_min, y_max;
+    Boundary(int xmin, int xmax, int ymin, int ymax) : x_min(xmin), x_max(xmax), y_min(ymin), y_max(ymax) {}
+};
 
+// Función para probar la inserción de puntos en el QuadTree
+void pruebaQuadTree() {
+    // Definir el límite del QuadTree (un cuadrado de 100x100)
+    Boundary limite(0, 100, 0, 100);
+    
+    // Crear el QuadTree con el límite
+    QuadTree quadtree(limite);
+
+    // Insertar algunos puntos
+    quadtree.insert(Point(10, 30));
+    quadtree.insert(Point(50, 75));
+    quadtree.insert(Point(70, 20));
+    quadtree.insert(Point(90, 90));
+
+    // Mostrar resultados
+    cout << "Puntos insertados en el QuadTree." << endl;
 }
+
+int main() {
+    pruebaQuadTree();
+    return 0;
+}
+
+
