8c86168f787c0d9be4f7b7c6d400dc32417c75ac
Khamarhack <143459006+Khamarhack@users.noreply.github.com>
2024-10-04 15:40:56 -0500

    Update quadtree.hxx

--- Arbol Quad-Tree/quadtree.hxx
+++ Arbol Quad-Tree/quadtree.hxx
@@ -1,3 +1,9 @@
+/******************************************
+Fecha: 1 octubre 2024
+Autor: Santiago Camargo
+Tema: Taller Arboles
+Estructuras de datos
+******************************************/
 #include <bits/stdc++.h>
 #include "quadtree.h"
 
