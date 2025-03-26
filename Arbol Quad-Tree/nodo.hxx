0e990f6c0339f8a51e8e31a6caa61e135415b71a
Khamarhack <143459006+Khamarhack@users.noreply.github.com>
2025-03-26 12:00:27 -0500

    Update nodo.hxx

--- Arbol Quad-Tree/nodo.hxx
+++ Arbol Quad-Tree/nodo.hxx
@@ -6,6 +6,10 @@ Estructuras de datos
 ******************************************/
 #include "Nodo.h"
 
+/**
+ * @brief Constructor por defecto de la clase Nodo.
+ * Inicializa los punteros a hijos en nullptr.
+ */
 template<class T>
 Nodo<T>::Nodo() {
     this->NW = nullptr;
@@ -14,6 +18,10 @@ Nodo<T>::Nodo() {
     this->SE = nullptr;
 }
 
+/**
+ * @brief Constructor que inicializa un nodo con un valor.
+ * @param val Par de valores que representa el dato del nodo.
+ */
 template<class T>
 Nodo<T>::Nodo(pair<T, T> val) {
     this->dato = val;
@@ -23,22 +31,36 @@ Nodo<T>::Nodo(pair<T, T> val) {
     this->SE = nullptr;
 }
 
+/**
+ * @brief Obtiene el dato almacenado en el nodo.
+ * @return Par de valores almacenado en el nodo.
+ */
 template<class T>
 pair<T, T> Nodo<T>::obtenerDato() {
     return this->dato;
 }
 
+/**
+ * @brief Establece un nuevo valor en el nodo.
+ * @param val Par de valores a almacenar en el nodo.
+ */
 template<class T>
 void Nodo<T>::fijarDato(pair<T, T> val) {
     this->dato = val;
 }
 
+/**
+ * @brief Calcula la altura del árbol a partir del nodo actual.
+ * @return Altura del árbol.
+ */
 template<class T>
 int Nodo<T>::altura() {
     if (NW == nullptr && NE == nullptr && SW == nullptr && SE == nullptr)
-        return 1;
+        return 1; // Un nodo sin hijos tiene altura 1
 
     int mayor = 0, actual;
+    
+    // Se verifica cada subárbol y se determina el de mayor altura
     if (NW != nullptr) {
         actual = this->NW->altura() + 1;
         if (actual > mayor)
@@ -63,12 +85,18 @@ int Nodo<T>::altura() {
     return mayor;
 }
 
+/**
+ * @brief Calcula el tamaño del árbol a partir del nodo actual.
+ * @return Número total de nodos en el árbol.
+ */
 template<class T>
 int Nodo<T>::tamano() {
     if (NW == nullptr && NE == nullptr && SW == nullptr && SE == nullptr)
-        return 1;
+        return 1; // Un nodo sin hijos cuenta como 1
 
     int cont = 1;  // Contamos el nodo actual
+    
+    // Se suman los tamaños de los subárboles si existen
     if (NW != nullptr)
         cont += this->NW->tamano();
     if (NE != nullptr)
@@ -81,6 +109,10 @@ int Nodo<T>::tamano() {
     return cont;
 }
 
+/**
+ * @brief Inserta un nuevo nodo en la estructura del árbol cuaternario.
+ * @param val Par de valores a insertar.
+ */
 template<class T>
 void Nodo<T>::insertar(pair<T, T> val) {
     /* Norte */
@@ -126,24 +158,26 @@ void Nodo<T>::insertar(pair<T, T> val) {
     }
 }
 
+/**
+ * @brief Busca un nodo con el valor especificado en el árbol.
+ * @param val Par de valores a buscar.
+ * @return Puntero al nodo encontrado o nullptr si no existe.
+ */
 template<class T>
 Nodo<T>* Nodo<T>::buscar(pair<T, T> val) {
     if (this->dato == val)
-        return this;
+        return this; // Nodo encontrado
+
     if (NW == nullptr && NE == nullptr && SW == nullptr && SE == nullptr)
-        return nullptr;
+        return nullptr; // No hay más nodos donde buscar
 
     if (this->dato.first < val.first) {
         if (this->dato.second < val.second) {
             if (this->NE != nullptr)
                 return this->NE->buscar(val);
-            else
-                return nullptr;
         } else {
             if (this->SE != nullptr)
                 return this->SE->buscar(val);
-            else
-                return nullptr;
         }
     }
 
@@ -151,19 +185,19 @@ Nodo<T>* Nodo<T>::buscar(pair<T, T> val) {
         if (this->dato.second < val.second) {
             if (this->NW != nullptr)
                 return this->NW->buscar(val);
-            else
-                return nullptr;
         } else {
             if (this->SW != nullptr)
                 return this->SW->buscar(val);
-            else
-                return nullptr;
         }
     }
 
-    return nullptr;
+    return nullptr; // Nodo no encontrado
 }
 
+/**
+ * @brief Realiza un recorrido en preorden e imprime los valores.
+ * Imprime el nodo actual, seguido de sus hijos en orden NW, NE, SW, SE.
+ */
 template<class T>
 void Nodo<T>::preOrden() {
     cout << "(" << this->dato.first << ',' << this->dato.second << ")" << endl;
@@ -177,6 +211,10 @@ void Nodo<T>::preOrden() {
         this->SE->preOrden();
 }
 
+/**
+ * @brief Realiza un recorrido en posorden e imprime los valores.
+ * Recorre primero los hijos en orden NW, NE, SW, SE, y luego imprime el nodo actual.
+ */
 template<class T>
 void Nodo<T>::posOrden() {
     if (this->NW != nullptr)
@@ -190,6 +228,7 @@ void Nodo<T>::posOrden() {
 
     cout << "(" << this->dato.first << ',' << this->dato.second << ")" << endl;
 }
+
 
 
 
