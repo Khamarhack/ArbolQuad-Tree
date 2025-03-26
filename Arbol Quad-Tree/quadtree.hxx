c01ad446244c07a68a0cead6db76ff41a65af2e6
Khamarhack <143459006+Khamarhack@users.noreply.github.com>
2025-03-26 12:11:20 -0500

    Update quadtree.hxx

--- Arbol Quad-Tree/quadtree.hxx
+++ Arbol Quad-Tree/quadtree.hxx
@@ -10,85 +10,85 @@ Estructuras de datos
 using namespace std;
 
 template<class T>
-Arbol<T>::Arbol()
-{
-    Nodo<T>* root = NULL;
+Arbol<T>::Arbol() {
+    this->raiz = nullptr;
 }
 
 template<class T>
-Arbol<T>::Arbol(pair<T,T> val)
-{
-    Nodo<T>* root =new  Nodo<T>(val);
-    this->raiz=root;
+Arbol<T>::Arbol(pair<T, T> val) {
+    this->raiz = new Nodo<T>(val);
 }
-/*
+
+// Destructor para liberar memoria
 template<class T>
-Arbol<T>::~Arbol()
-{
-    delete(this->raiz);
+Arbol<T>::~Arbol() {
+    eliminarTodo(this->raiz);
 }
-*/
+
+// Función auxiliar para eliminar nodos recursivamente
 template<class T>
-bool Arbol<T>:: esVacio()
-{
-    if(this->raiz==NULL)
-        return true;
-    return false;
+void Arbol<T>::eliminarTodo(Nodo<T>* nodo) {
+    if (nodo == nullptr) return;
+    eliminarTodo(nodo->NW);
+    eliminarTodo(nodo->NE);
+    eliminarTodo(nodo->SW);
+    eliminarTodo(nodo->SE);
+    delete nodo;
 }
 
 template<class T>
-pair<T,T> Arbol<T>:: obtenerRaiz()
-{
-    return this->raiz;
+bool Arbol<T>::esVacio() {
+    return this->raiz == nullptr;
 }
 
 template<class T>
-void Arbol<T>::fijarRaiz(Nodo<T>* root)
-{
+pair<T, T> Arbol<T>::obtenerRaiz() {
+    if (this->raiz != nullptr)
+        return this->raiz->obtenerDato();
+    throw runtime_error("El árbol está vacío.");
+}
+
+template<class T>
+void Arbol<T>::fijarRaiz(Nodo<T>* root) {
     this->raiz = root;
 }
 
 template<class T>
-int Arbol<T>:: altura()
-{
-    return this->raiz->altura();
+int Arbol<T>::altura() {
+    return this->raiz ? this->raiz->altura() : 0;
 }
 
 template<class T>
-int Arbol<T>:: tamano()
-{
-    return this->raiz->tamano();
+int Arbol<T>::tamano() {
+    return this->raiz ? this->raiz->tamano() : 0;
 }
 
 template<class T>
-void Arbol<T>:: insertar(pair<T,T> val)
-{
-    this->raiz->insertar(val);
-}
-
-/*
-template<class T>
-bool Arbol<T>:: eliminar(T val)
-{
-    return this->raiz->eliminar(val);
-}
-*/
-template<class T>
-Nodo<T>* Arbol<T>::  buscar(pair<T,T> val)
-{
-    return this->raiz->buscar(val);
+void Arbol<T>::insertar(pair<T, T> val) {
+    if (this->raiz)
+        this->raiz->insertar(val);
+    else
+        this->raiz = new Nodo<T>(val);
 }
 
 template<class T>
-void Arbol<T>:: preOrden()
-{
-    this->raiz->preOrden();
+Nodo<T>* Arbol<T>::buscar(pair<T, T> val) {
+    return this->raiz ? this->raiz->buscar(val) : nullptr;
 }
 
+template<class T>
+void Arbol<T>::preOrden() {
+    if (this->raiz)
+        this->raiz->preOrden();
+    else
+        cout << "El árbol está vacío." << endl;
+}
 
 template<class T>
-void Arbol<T>:: posOrden()
-{
-    this->raiz->posOrden();
+void Arbol<T>::posOrden() {
+    if (this->raiz)
+        this->raiz->posOrden();
+    else
+        cout << "El árbol está vacío." << endl;
 }
 
